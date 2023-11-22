340daccc46d8040be27c0e73c496b8590523e0e1
Immanuel Raj <iamimmanuelraj@gmail.com>
2023-11-22 16:03:49 +0530

    jasmine_sprout: Switch to QTI v1.2 Bootctl

--- device.mk
+++ device.mk
@@ -94,11 +94,9 @@ PRODUCT_COMPRESSED_APEX := false
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-impl.recovery \
-    android.hardware.boot@1.0-service \
-    bootctrl.sdm660 \
-    bootctrl.sdm660.recovery
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
